1ac7eba307967c98fb5fe4a0550b3f46b834de52
punkish <punk.kish@gmail.com>
2024-10-23 17:00:43 +0200

    tweaked recentering and styles of slidebar

--- src/leaflet.slidebar.js
+++ src/leaflet.slidebar.js
@@ -19,21 +19,17 @@ const Slidebar = L.Control.extend({
             let targetSize;
 
             if (evt.value > this.options.doubleThreshold) {
-                //this.toggleSize('full');
                 targetSize = 'full';
             } 
             else {
                 switch (this._size) {
                     case 'closed':
-                        //this.toggleSize('quarter');
                         targetSize = 'quarter';
                         break;
                     case 'quarter':
-                        //this.toggleSize('half');
                         targetSize = 'half';
                         break;
                     case 'half':
-                        //this.toggleSize('full');
                         targetSize = 'full';
                         break;
                 }
@@ -46,21 +42,17 @@ const Slidebar = L.Control.extend({
             let targetSize;
 
             if (evt.value > this.options.doubleThreshold) {
-                //this.toggleSize('closed');
                 targetSize = 'closed';
             } 
             else {
                 switch (this._size) {
                     case 'full':
-                        //this.toggleSize('half');
                         targetSize = 'half';
                         break;
                     case 'half':
-                        //this.toggleSize('quarter');
                         targetSize = 'quarter';
                         break;
                     case 'quarter':
-                        //this.toggleSize('closed');
                         targetSize = 'closed';
                         break;
                 }
@@ -74,17 +66,18 @@ const Slidebar = L.Control.extend({
 
     toggleSize: function(targetSize) {
         const el = document.querySelector('#leaflet-slidebar');
-        const sizes = [ 'closed', 'quarter', 'half', 'full' ];
-        sizes.forEach(s => el.classList.remove(s));
-        el.classList.add(targetSize);
-        this._size = targetSize;
-        
-        // Create the event
-        //const event = new CustomEvent("sizeToggled", { "detail": "Info window size was toggled" });
 
-        // Dispatch/Trigger/Fire the event
-        //this.dispatchEvent(event);
-        //this.fire("sizeToggled")
+        if (el.classList.contains(targetSize)) {
+
+        }
+        else {
+            const sizes = [ 'closed', 'quarter', 'half', 'full' ];
+            sizes.forEach(s => el.classList.remove(s));
+            el.classList.add(targetSize);
+            this._size = targetSize;
+        }
+        
+        this.reCenter(this._newCenter);
     },
 
     onAdd: function(map) {
@@ -97,22 +90,14 @@ const Slidebar = L.Control.extend({
             this._div.id = 'leaflet-slidebar';
             this._size = this.options.state;
 
-            L.DomUtil.create('hr', '', this._div);
-            //L.DomEvent.on(hr, 'click', this.toggleSize, this);
+            const nav = L.DomUtil.create('nav', '', this._div);
+            const hr = L.DomUtil.create('hr', '', nav);
+            // L.DomEvent.on(hr, 'click', this.toggleSize, this);
 
-            // TO FIX: turned off navbar construction for now because of the 
-            // conflict between touch and click events.
-
-            // this._div.innerHTML = `
-            //     <nav>
-            //         <ul class="right">
-            //             <li><button name="full" title="maximize the info window" data-lat="" data-lng="">&#9633;</button></li>
-            //             <li><button name="half" title="set info window to half size" data-lat="" data-lng="">&lrtri;</button></li>
-            //             <li><button name="quarter" title="set info window to a quarter size" data-lat="" data-lng="">&mdash;</button></li>
-            //             <li><button name="closed" title="close the info window" data-lat="" data-lng="">&times;</button></li>
-            //         </ul>
-            //     </nav>`;
-            //this._div.innerHTML += '<main></main>';
+            const closeBtn = L.DomUtil.create('button', 'close-btn', nav);
+            const toggleClose = (e) => this.toggleSize('closed');
+            L.DomEvent.on(closeBtn, 'click', toggleClose, this);
+            L.DomEvent.on(closeBtn, 'touchend', toggleClose, this);
             L.DomUtil.create('main', '', this._div);
         }
 
@@ -127,34 +112,62 @@ const Slidebar = L.Control.extend({
     },
 
 /*
-       ◀──────────  x   ─────────▶                                                                                                 max_x,
-                                                                                                                                   max_y 
-  ▲    ┌─────────────────────────┐     ┌─────────────────────────┐     ┌─────────────────────────┐     ┌─────────────────────────┐
-  │    │                         │     │                         │     │                         │     │                         │
-  │    │                         │     │                         │     │                         │     │                         │
-  │    │           ▲•            │     │                         │     │                         │     │                         │
-  │    │           │          o  │     │           ▲•         o  │     │                      o  │     │                      o ◀┼─ clicked
-  │    │┌──────────┼────────────┐│     │           │             │     │                         │     │                         │  marker 
-  │    ││          │            ││     │     0.25y │             │     │           ▲•            │     │                         │
-  │    ││    0.375y│            ││     │           │             │     │     0.125y│             │     │                         │
-  │    ││          │            ││     │┌──────────┼────────────┐│     │           │             │     │                         │
-  │    ││          ▼x ◀─────────┼┼─────┼┼──────────▼x ──────────┼┼─────┼───────────▼x ───────────┼─────┼─────────── • ───────────┼─ center 
-       ││                       ││     ││                       ││     │                         │     │                         │
-  y    ││                       ││     ││                       ││     │                         │     │                         │
-       ││                       ││     ││                       ││     │                         │     │                         │
-  │    ││                       ││     ││                       ││     │                         │     │        0y               │
-  │    ││                       ││     ││                       ││     │┌───────────────────────┐│     │                         │
-  │    ││                       ││     ││                       ││     ││                       ││     │                         │
-  │    ││       sliding         ││     ││                       ││     ││                       ││     │                         │
-  │    ││        panel          ││     ││                       ││     ││                       ││     │                         │
-  │    ││                       ││     ││                       ││     ││                       ││     │                         │
-  ▼    └┴───────────────────────┴┘     └┴───────────────────────┴┘     └┴───────────────────────┴┘     └─────────────────────────┘
-     min_x,
-     min_y
+                                                                                
+        ◀──────────  x   ─────────▶                                             
+                                                                     max_x,     
+   ▲    ┌─────────────────────────┐     ┌─────────────────────────┐  max_y      
+   │    │                         │     │                         │             
+   │    │                         │     │                         │             
+   │    │           ▲•            │     │                         │             
+   │    │           │          o  │     │           ▲•         o ◀┼──  clicked  
+   │    │┌──────────┼────────────┐│     │           │             │     marker  
+   │    ││          │            ││     │     0.25y │             │             
+   │    ││    0.375y│            ││     │           │             │             
+   │    ││          │            ││     │┌──────────┼────────────┐│             
+   │    ││          ▼x ◀─────────┼┼─────┼┼──────────▼x ──────────┼┼──   center  
+        ││                       ││     ││                       ││             
+   y    ││                       ││     ││                       ││             
+        ││                       ││     ││                       ││             
+   │    ││                       ││     ││                       ││             
+   │    ││                       ││     ││                       ││             
+   │    ││                       ││     ││                       ││             
+   │    ││                       ││     ││                       ││             
+   │    ││       sliding         ││     ││                       ││             
+   │    ││        panel          ││     ││                       ││             
+   ▼    └┴───────────────────────┴┘     └┴───────────────────────┴┘             
+      min_x,       FULL                           HALF                          
+      min_y                                                                     
+                                                                                
+                                                                                
+       ◀──────────  x   ─────────▶                                  max_x,      
+                                                                    max_y       
+  ▲    ┌─────────────────────────┐      ┌─────────────────────────┐             
+  │    │                         │      │                         │             
+  │    │                         │      │                         │             
+  │    │                         │      │                         │             
+  │    │                      o  │      │                      o ◀┼──  clicked  
+  │    │                         │      │                         │     marker  
+  │    │           ▲•            │      │                         │             
+  │    │     0.125y│             │      │                         │             
+  │    │           │             │      │                         │             
+  │    │           ▼x ◀──────────┼──────┼────────── • ────────────┼──   center  
+       │                         │      │                         │             
+  y    │                         │      │                         │             
+       │                         │      │                         │             
+  │    │                         │      │       0y                │             
+  │    │┌───────────────────────┐│      │                         │             
+  │    ││                       ││      │                         │             
+  │    ││                       ││      │                         │             
+  │    ││       sliding         ││      │                         │             
+  │    ││        panel          ││      │                         │             
+  ▼    └┴───────────────────────┴┘      └─────────────────────────┘             
+     min_x,    QUARTER                         CLOSED                           
+     min_y                                                                      
+                                                                                
 */
 
-    reCenter: function(latlng) {
-        const centerPx = this._map.latLngToLayerPoint(latlng);
+    reCenter: function() {
+        const centerPx = this._map.latLngToLayerPoint(this._newCenter);
         const newCenterPx = [];
         const { min, max } = this._map.getPixelBounds();
 
@@ -183,23 +196,10 @@ const Slidebar = L.Control.extend({
     },
 
     update: async function({ content, latlng }) {
-        
-        //this._div.innerHTML = content;
         const main = this._div.querySelector('#leaflet-slidebar main');
         main.innerHTML = content;
         this._newCenter = latlng;
-
-        if (this._size === 'closed') {
-            this.toggleSize('full');
-        }
-
-        this.reCenter(this._newCenter);
-        
-        // const buttons = document.querySelectorAll('#leaflet-slidebar ul.right button');
-        // buttons.forEach((button) => {
-        //     button.dataset.lat = latlng.lat;
-        //     button.dataset.lng = latlng.lng;
-        // });
+        this.toggleSize('full');
 
         return this;
     },
